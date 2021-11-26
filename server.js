aeb677e7e4567414ba4c5989dd8db5ab81f394d2
Norm Ross <norm.ross@senstar.com>
2021-11-26 14:58:50 -0500

    After tutorial

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require("express"),
-  bodyParser = require("body-parser");
+  bodyParser = require("body-parser"),
+  swaggerJsdoc = require("swagger-jsdoc"),
+  swaggerUi = require("swagger-ui-express");
 
 const app = express();
 app.use(
@@ -10,6 +12,40 @@ app.use(
 app.use(bodyParser.json());
 
 app.use("/books", require("./routes/books"));
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Wizard API",
+      version: "0.1.0",
+      description:
+        "The game of Wizard",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "InspiroBot",
+        url: "https://github.com/inspirobot",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, { explorer: true })
+);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
